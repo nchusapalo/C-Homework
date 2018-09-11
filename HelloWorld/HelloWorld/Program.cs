d6362839864bae017d2fa61f862d65f9fc9fe0ae
nchusapalo <nchusapalo@gmail.com>
2018-09-11 15:48:45 +0700

    Insert first code

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("My Name is ToshibaLabtop and What's your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadLine();
+        }
+    }
+}
